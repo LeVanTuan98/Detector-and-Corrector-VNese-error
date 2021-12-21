e25153140198e05bf0f93adda77eb54b18d3ace7
LeVanTuan98 <leanhtuan.311098@gmail.com>
2021-12-22 01:23:19 +0700

    updat22-12

--- main.py
+++ main.py
@@ -20,21 +20,26 @@ def index():
 #         # print(pred)
 #     return render_template('index.html')
 
-@app.route('/check', methods=['POST'])
+@app.route('/check', methods=['POST', 'GET'])
 def checking():
     # html -> py
     if request.method == 'POST':
         raw_doc = request.form['input']
         print('raw: ', raw_doc)
         doc = cleaner.clean_html(raw_doc)
-        checked_doc = predict(model, doc)
-        # checked_doc = [('aa', 1, ('a', 'b', 'c')), ('bb', 0, ())]
+        global checked_doc
+        # checked_doc = predict(model, doc)
+        checked_doc = [('aa', 1, ('a', 'b', 'c')), ('bb', 0, ())]
         print('check: ', checked_doc)
+        num_words = 2
+    else:
+        num_words = int(request.args.get('n'))
     # py -> html
-    return render_template('checking.html', words=checked_doc)
+    print(num_words)
+    return render_template('checking.html', words=checked_doc, n=num_words)
 
 
 if __name__ == '__main__':
-    global model
-    model = load_model()
+    # global model
+    # model = load_model()
     app.run(debug=True)
